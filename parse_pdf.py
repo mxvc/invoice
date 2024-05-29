b1e40f52b4d717c53da2162b515024643fb886a1
jiangtao <410518072@qq.com>
2024-05-29 23:07:40 +0800

    解析大写金额

--- parse_pdf.py
+++ parse_pdf.py
@@ -1,11 +1,4 @@
-import json
-
-import fitz  # PyMuPDF
-import re
-
 import util
-from parse_pdf_10 import parse_pdf_10
-from parse_pdf_31 import parse_pdf_31
 from util import pdf_read_text
 
 
@@ -23,18 +16,3 @@ def parse_pdf(pdf_path, info):
             item['总金额'] = util.chinese2digits(rs)
 
 
-
-
-
-
-if __name__ == '__main__':
-    text = pdf_read_text("D:/OneDrive/当前-发票/ERR_1.pdf")
-    arr = text.split('\n')
-
-    for item in arr:
-        # '贰拾肆圆零柒分 ¥24.07'
-        if util.contains_chinese_currency(item):
-            rs = util.find_chinese_currency(item)
-
-            print(item)
-    print(text)
