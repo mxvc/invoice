9940b0a3765480c2d467351eb542f701842bbc46
moon <410518072@qq.com>
2024-07-27 22:49:30 +0800

    优化

--- util.py
+++ util.py
@@ -1,5 +1,4 @@
 import re
-from datetime import datetime
 
 import fitz  # PyMuPDF
 import pdfplumber
@@ -75,75 +74,15 @@ def pdf_read_text(path):
     return rs
 
 
-def now_str():
-    now = datetime.now()  # current date and time
-    date_time = now.strftime("%Y-%m-%d, %H:%M:%S")
-    print("date and time:", date_time)
-    return date_time
-
-
-# 是否中文大写金额
-def is_chinese_currency(text):
-    pattern = re.compile(r"^(?:[零壹贰叁肆伍陆柒捌玖]|\d)+(?:[元角分])+$")
-    return bool(pattern.match(text))
-
-
-def contains_chinese_currency(text):
-    pattern = re.compile(r"(?:[零壹贰叁肆伍陆柒捌玖]|\d)+(?:[元角分])")
-    return bool(pattern.search(text))
-
-
-def find_chinese_currency(text):
-    pattern = r'[\u4e00-\u9fa5]+'
-
-    rs = re.findall(pattern, text)
-    if rs[0]:
-        rs = rs[0]
-    else:
-        return None
-
-    print("过滤中文大写：", rs)
-    if "元" in rs:
-        return rs
-    return None
-
-
-def chinese_to_numerals(chinese_str):
-    chinese_numerals = {'零': 0, '壹': 1, '贰': 2, '叁': 3, '肆': 4,
-                        '伍': 5, '陆': 6, '柒': 7, '捌': 8, '玖': 9}
-    chinese_units = {'圆': 1, '角': 0.1, '分': 0.01}
-
-    num = 0
-    temp = 0  # 临时存储中间结果
-
-    for char in chinese_str:
-        if char in chinese_units:
-            unit = chinese_units[char]
-            num += temp * unit
-            temp = 0
-        elif char in chinese_numerals:
-            temp += chinese_numerals[char]
-        elif char == '拾':
-            temp *= 10
-        elif char == '佰':
-            temp *= 100
-        elif char == '仟':
-            temp *= 1000
-        elif char == '万':
-            temp *= 10000
-        elif char == '亿':
-            temp *= 100000000
-
-    return num
-
 def is_number(text):
     try:
-        number = float(text)
+        float(text)
         print("The text is a float.")
     except ValueError:
         print("The text is not a number.")
         return False
-    return  True
+    return True
+
 
 def find_numbers(text):
     # 正则表达式匹配整数和小数
